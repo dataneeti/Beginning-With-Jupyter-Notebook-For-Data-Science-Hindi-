ca158a9ccefb38ee5594447ebc1553ff499672d2
dataneeti <61863742+dataneeti@users.noreply.github.com>
2020-06-03 18:00:12 +0530

    Add files via upload
    03-06-2020
    Original uploads

---
+++ Python_file/Factorial.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def factorial(num):
+    if num == 1:
+        return 1
+    else:
+        return (num * factorial(num-1))
+
+
+# In[ ]:
+
+
+factorial(10)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
